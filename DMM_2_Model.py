365b5a34ebbcd5b639332f2dfed03b1bb7007ba9
AOS-RexWang <rex.wang@tw.aosmd.com>
2022-08-26 09:21:37 +0800

    DMM_Model V1.0.3 DMM_2_Model V1.0.3
    Add Model Name
    Add command: CMD_Set_Current_Terminals

--- DMM_2_Model.py
+++ DMM_2_Model.py
@@ -1,7 +1,7 @@
 """
  * @File       : DMM_Model.py
- * @Version    : V1.0.2
- * @Date       : Aug 19, 2022
+ * @Version    : V1.0.3
+ * @Date       : Aug 26, 2022
  * @Brief      : Child class of DMM.
  * @Author     : Rex Wang
  * @Last editor: Rex Wang
@@ -11,12 +11,13 @@
 from Library.INSTR import DMM
 
 class Keysight_34461(DMM):
+    Model_Name                     = "Keysight_34461"
+    CMD_Config_IMode               = "CONF:CURR:%s %.4f, %.3f"
+    CMD_Config_IMode_Auto_Range    = "CONF:CURR:%s AUTO"    
     CMD_Config_VMode               = "CONF:VOLT:%s %s, %.3f"
     CMD_Config_VMode_Auto_Range    = "CONF:VOLT:%s AUTO"
-    CMD_Config_IMode               = "CONF:CURR:%s %.4f, %.3f"
-    CMD_Config_IMode_Auto_Range    = "CONF:CURR:%s AUTO"
     CMD_Measure_VNPLC              = "SENS:VOLT:DC:NPLC %d"
     CMD_Measure_INPLC              = "SENS:CURR:DC:NPLC %d"
     CMD_Measure_Voltage            = "MEAS:VOLT:%s?"
     CMD_Measure_Current            = "MEAS:CURR:%s?"
-    #CMD_MEAS_TEMP = "MEAS:TEMP:%s? %s, MAX,(@%d)"
+    CMD_Set_Current_Terminals      = "SENSe:CURRent:%s:TERMinals %d"
