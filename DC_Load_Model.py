87c870be650296b5cbdbfa01f2d12383ba1c9080
Rex Wang <rex.wang@tw.aosmd.com>
2023-02-23 14:37:37 +0800

    DC_Load_Model V1.1.2 Add DC load Model: Chroma_63630_80_60, Chroma_63640_80_80 Add current range

--- DC_Load_Model.py
+++ DC_Load_Model.py
@@ -1,6 +1,6 @@
 """
  * @File       : DC_Load_Model.py
- * @Version    : V1.1.1
+ * @Version    : V1.1.2
  * @Date       : July 01, 2022
  * @Brief      : Child class of DC Load.
  * @Author     : Rex Wang
@@ -10,7 +10,9 @@
 
 from Library.INSTR import DC_Load
 
-class Chroma_6312A(DC_Load):
+class Chroma_63103(DC_Load):
+    Model_Name                              = "Chroma_63103"
+    Current_Range                           = {"CCL": 6, "CCH": 60}
     CMD_Get_Load_State                      = "LOAD?"
     CMD_Measure_Voltage                     = "MEASure:VOLTage?"
     CMD_Measure_Current                     = "MEASure:CURRent?"
@@ -30,7 +32,9 @@ class Chroma_6312A(DC_Load):
     CMD_Set_Static_Current                  = "CURRent:STATic:L1 %.3f"
     CMD_Set_Static_Current_Slew_Rate        = "CURRent:STATic:%s %.3f"
 
-class Chroma_63600_Series(DC_Load):
+class Chroma_63630_80_60(DC_Load):
+    Model_Name                              = "Chroma_63630_80_60"
+    Current_Range                           = {"CCL": 0.6, "CCM": 6, "CCH": 60}
     CMD_Get_Load_State                      = "LOAD?"
     CMD_Measure_Voltage                     = "MEASure:VOLTage?"
     CMD_Measure_Current                     = "MEASure:CURRent?"
@@ -50,3 +54,24 @@ class Chroma_63600_Series(DC_Load):
     CMD_Set_Static_Current                  = "CURRent:STATic:L1 %.3f"
     CMD_Set_Static_Current_Slew_Rate        = "CURRent:STATic:%s %.3f"
 
+class Chroma_63640_80_80(DC_Load):
+    Model_Name                              = "Chroma_63640_80_80"
+    Current_Range                           = {"CCL": 0.8, "CCM": 8, "CCH": 80}
+    CMD_Get_Load_State                      = "LOAD?"
+    CMD_Measure_Voltage                     = "MEASure:VOLTage?"
+    CMD_Measure_Current                     = "MEASure:CURRent?"
+    CMD_Set_Mode                            = "MODE %s"
+    CMD_Set_Channel                         = "CHANnel %d"
+    CMD_Set_Dynamic_Current                 = "CURRent:DYNamic:L%d %.3f"
+    CMD_Set_Dynamic_Current_Slew_Rate       = "CURRent:DYNamic:%s %.3f"
+    CMD_Set_Dynamic_Current_Period          = "CURRent:DYNamic:T%d %f"
+    CMD_Set_Load_Short_Off                  = "LOAD:SHORt OFF"
+    CMD_Set_Load_Short_On                   = "LOAD:SHORt ON"
+    CMD_Set_Load_State_Off                  = "Load OFF"
+    CMD_Set_Load_State_On                   = "LOAD ON"
+    CMD_Set_Load_State_All_Off              = ":ABORt"
+    CMD_Set_Load_State_All_On               = ":RUN"
+    CMD_Set_Resistance                      = "RESistance:STATic:L1 %.3f"
+    CMD_Set_Resistance_Slew_Rate            = ""
+    CMD_Set_Static_Current                  = "CURRent:STATic:L1 %.3f"
+    CMD_Set_Static_Current_Slew_Rate        = "CURRent:STATic:%s %.3f"
